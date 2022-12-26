ebd03f47ba92e5328fd878b6c6cb8fcedbc67fa5
hherrerob <hherrerob@gmail.com>
2022-12-26 15:42:55 +0100

    Remove useless comment

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,4 @@
 <template>
-  <!--
-  <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img src="./assets/electron.svg" class="logo electron" alt="Electron logo" />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Electron + Vite + Vue" />
-  <div class="flex-center">
-    Place static files into the <code>/public</code> folder
-    <img style="width:5em;" src="/node.svg" alt="Node logo">
-  </div>
-  -->
-
   <router-view />
 </template>
 
