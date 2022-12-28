0177d1856f36c322e97a103467fde5d22236581e
hherrerob <hherrerob@gmail.com>
2022-12-28 01:32:16 +0100

    Solves minor bug

--- electron/main/database/models/trace.ts
+++ electron/main/database/models/trace.ts
@@ -1,12 +1,11 @@
 import { Column, Model, DataType, ForeignKey } from 'sequelize-typescript'
 import Session from './session'
 
-
 export default class Trace extends Model {
     @Column(DataType.DATE)
     declare received_time_utc: Date
 
     @ForeignKey(() => Session)
-    @Column(DataType.DATE)
+    @Column(DataType.INTEGER)
     declare session_id
 }
