aee73a50ffea423a984af312e091d81ea57300fd
hherrerob <hherrerob@gmail.com>
2022-12-28 22:25:02 +0100

    Add navbar components

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dashboard-navbar">
+    <b-navbar type="dark" variant="dark">
+      <slot />
+    </b-navbar>
+  </div>
+</template>
+
+<script setup lang="tsx">
+
+</script>
+
+<style>
+.dashboard-navbar {
+  position: sticky;
+  top: 0;
+  z-index: 1;
+
+  border-bottom: 1px solid red;
+}
+</style>
