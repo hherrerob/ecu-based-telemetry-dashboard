658008b8966d392b8f43f36c385d0aab80a319dc
hherrerob <hherrerob@gmail.com>
2022-12-30 23:15:42 +0100

    Add apex charts to main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import './samples/node-api'
 import { router } from './routes'
 import BootstrapVue3 from 'bootstrap-vue-3'
+import VueApexCharts from "vue3-apexcharts";
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -12,6 +13,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 createApp(App)
     .use(router)
     .use(BootstrapVue3)
+    .use(VueApexCharts)
     .mount('#app')
     .$nextTick(() => {
         postMessage({ payload: 'removeLoading' }, '*')
