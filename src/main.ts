9b2aa22739bd772061f7c95493d0187a213b6648
hherrerob <hherrerob@gmail.com>
2023-01-02 04:23:38 +0100

    Remove node-api from main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import "./style.css"
 import App from './App.vue'
-import './samples/node-api'
 import { router } from './routes'
 import BootstrapVue3 from 'bootstrap-vue-3'
 import VueApexCharts from "vue3-apexcharts";
