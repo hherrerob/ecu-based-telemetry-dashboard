cb87bd488453f7cab2caf557de9e91219e047fad
hherrerob <hherrerob@gmail.com>
2023-01-02 21:16:34 +0100

    Add custom styles

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import "./style.css"
 import App from './App.vue'
 import { router } from './routes'
 import BootstrapVue3 from 'bootstrap-vue-3'
@@ -8,6 +7,9 @@ import VueApexCharts from "vue3-apexcharts";
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Now custom styles
+import "./style.scss"
 
 createApp(App)
     .use(router)
