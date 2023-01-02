3a16d6800e23aeeb45a7fa47b3f9de965573daae
hherrerob <hherrerob@gmail.com>
2023-01-02 04:55:09 +0100

    Add a new constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
 export const STEP = 100 // ms
+export const NUMBER_OF_ITEMS_TO_KEEP_IN_MEMORY = 20
 
 export const GET_TELEMETRY_DATA_ROUTE = 'get-telemetry-data'
 export const GET_SESSIONS_ROUTE = 'get-sessions'
