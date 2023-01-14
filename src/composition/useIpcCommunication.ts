3001aad5b1d47a608d6a4fff85bf534f294e3b29
hherrerob <hherrerob@gmail.com>
2023-01-14 23:58:03 +0100

    Add parameters as nullables

--- src/composition/useIpcCommunication.ts
+++ src/composition/useIpcCommunication.ts
@@ -1,11 +1,11 @@
 import { ipcRenderer } from 'electron'
 import { GET_SESSIONS_ROUTE, GET_TELEMETRY_DATA_ROUTE, PURGE_DATABASE_ROUTE, SET_DATABASE_ROUTE } from '../constants'
 
-const setDatabase = (fullDatabasePath: string, databaseName: string) => {
+const setDatabase = (fullDatabasePath: Nullable<String>, databaseName: Nullable<String>) => {
     return ipcRenderer.invoke(SET_DATABASE_ROUTE, [fullDatabasePath, databaseName])
 }
 
-const purgeDatabase = (fullDatabasePath: string, databaseName: string) => {
+const purgeDatabase = (fullDatabasePath: Nullable<String>, databaseName: Nullable<String>) => {
     return ipcRenderer.invoke(PURGE_DATABASE_ROUTE, [fullDatabasePath, databaseName])
 }
 
