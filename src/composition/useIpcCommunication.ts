9a6c486e07291136c7879bf23e35350b32633ac5
hherrerob <hherrerob@gmail.com>
2023-01-14 21:26:35 +0100

    Add purge ipcCommunication function

--- src/composition/useIpcCommunication.ts
+++ src/composition/useIpcCommunication.ts
@@ -1,8 +1,12 @@
 import { ipcRenderer } from 'electron'
-import { GET_SESSIONS_ROUTE, GET_TELEMETRY_DATA_ROUTE, SET_DATABASE_ROUTE } from '../constants'
+import { GET_SESSIONS_ROUTE, GET_TELEMETRY_DATA_ROUTE, PURGE_DATABASE_ROUTE, SET_DATABASE_ROUTE } from '../constants'
 
 const setDatabase = (fullDatabasePath: string, databaseName: string) => {
     return ipcRenderer.invoke(SET_DATABASE_ROUTE, [fullDatabasePath, databaseName])
+}
+
+const purgeDatabase = (fullDatabasePath: string, databaseName: string) => {
+    return ipcRenderer.invoke(PURGE_DATABASE_ROUTE, [fullDatabasePath, databaseName])
 }
 
 const getTelemetryData = (sessionId: number) => {
@@ -13,4 +17,4 @@ const getSessions = () => {
     return ipcRenderer.invoke(GET_SESSIONS_ROUTE, [])
 }
 
-export { setDatabase, getTelemetryData, getSessions }
+export { setDatabase, getTelemetryData, getSessions, purgeDatabase }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,4 @@ export const NUMBER_OF_ITEMS_TO_KEEP_IN_MEMORY = 20
 export const GET_TELEMETRY_DATA_ROUTE = 'get-telemetry-data'
 export const GET_SESSIONS_ROUTE = 'get-sessions'
 export const SET_DATABASE_ROUTE = 'set-database'
+export const PURGE_DATABASE_ROUTE = 'purge-database'
